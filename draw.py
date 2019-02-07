2ec7520c976712da54a5f6494b633b18efd03a50
ecrystale <elee19@stuy.edu>
2019-02-07 12:01:30 -0500

    fix?

--- draw.py
+++ draw.py
@@ -3,12 +3,12 @@ from display import *
 def draw_line( x0, y0, x1, y1, screen, color ):
     x=x0
     y=y0
-    A=x1-x0
-    B=y1-y0
+    A=y1-y0
+    B=-1*(x1-x0)
     d=2*A+B
-    while x<=x1:
+    while (x<=x1):
         plot(screen,color,x,y)
-        if d>0:
+        if (d>0):
             y+=1
             d+=2*B
         x+=1
--- main.py
+++ main.py
@@ -6,9 +6,10 @@ color = [ 0, 255, 0 ]
 
 x0=0
 y0=0
-x1=500
-y1=100
+x1=234
+y1=3232
 draw_line( x0, y0, x1, y1, screen, color )
+draw_line( x0, y0, 1000, 1000, screen, color )
 
 display(screen)
 save_extension(screen, 'img.png')
